af67026c73d4377748c2165e3144f4f2bc2d59fb
Austin Fuller <auxchar@gmail.com>
2015-12-12 04:15:33 -0600

    That's indented weird, too...

--- src/main/java/auxchar/mods/chathider/ChatHider.java
+++ src/main/java/auxchar/mods/chathider/ChatHider.java
@@ -13,8 +13,8 @@ public class ChatHider {
     public static final String MODID = "chathider";
     public static final String VERSION = "1.0.0-1.8-11.14.4.1563";
 
-	@Instance(value = "chathider")
-	public static ChatHider instance;
+    @Instance(value = "chathider")
+    public static ChatHider instance;
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
