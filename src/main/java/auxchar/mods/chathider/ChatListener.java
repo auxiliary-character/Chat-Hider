c6e4e797c31ba2f685be53128fcd7551443fa031
Austin Fuller <auxchar@gmail.com>
2015-12-12 04:06:11 -0600

    That's indented weird.

--- src/main/java/auxchar/mods/chathider/ChatListener.java
+++ src/main/java/auxchar/mods/chathider/ChatListener.java
@@ -7,7 +7,7 @@ import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ChatListener {
-	private static Pattern snitch = Pattern.compile("^ \\* ([a-zA-Z0-9_]+) (?:entered|logged out in|logged in to) snitch at \\S* \\[(\\S+) ([-]?[0-9]+) ([-]?[0-9]+) ([-]?[0-9]+)\\]");
+    private static Pattern snitch = Pattern.compile("^ \\* ([a-zA-Z0-9_]+) (?:entered|logged out in|logged in to) snitch at \\S* \\[(\\S+) ([-]?[0-9]+) ([-]?[0-9]+) ([-]?[0-9]+)\\]");
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent e) {
         Matcher m = snitch.matcher(e.message.getUnformattedText());
